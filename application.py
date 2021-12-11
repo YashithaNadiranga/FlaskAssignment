a62379f692cd78733d1117a9532d4c636460dba6
Yashitha Nadiranga <yasitha.online100@gmail.com>
2021-12-11 16:38:41 +0530

    init

---
+++ application.py
@@ -0,0 +1,33 @@
+from flask import Flask,render_template,request, make_response
+from flask import jsonify
+from flask_pymongo import PyMongo
+from flask_pymongo import ObjectId
+
+application = Flask(__name__)
+application.config["MONGO_URI"] = "mongodb+srv://feanixlabs:feanixlabs@cluster0.ksxrr.mongodb.net/loan?retryWrites=true&w=majority"
+mongodb_client = PyMongo(application)
+db = mongodb_client.db
+
+@application.route('/', methods=["GET","POST"])
+def route_home():
+    return render_template('index.html')
+
+@application.route('/deposit', methods=["GET","POST"])
+def route_deposit():
+    if request.method =='POST':
+        name = request.form['name']
+        amount = request.form['amount']
+        db.users.insert_one({"name": name, "amount": int(amount)})
+    return render_template('deposit.html')
+
+@application.route('/getloan', methods=["GET","POST"])
+def route_getloan():
+    return render_template('getloan.html')
+
+@application.route('/payloan', methods=["GET","POST"])
+def route_payloan():
+    return render_template('payloan.html')
+
+
+if __name__ == '__main__':
+    application.run(debug=True)
